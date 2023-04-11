de7fcb58e36c4c7af6bcb99c56a98907b9566608
Hybrinter <102422753+Hybrinter@users.noreply.github.com>
2023-04-11 15:45:24 -0500

    Add files via upload

--- Visualization Vestigo/main.cpp
+++ Visualization Vestigo/main.cpp
@@ -18,25 +18,36 @@
 #undef main
 #include <fstream>
 #include <vector>
-#include <tuple>
+#include <sys/stat.h>
 
 using std::cout;
 using std::cin;
 using std::endl;
 
+
 // Definitions
 const double inch_to_meter = 0.0254;
 
 // Function to get dimensions of room from user input
-void getDimensions(double& length, double& width)
+double* getDimensions()
 {
-    std::cout << "Enter the length of the room in feet: ";
+    double length;
+    double width;
+
+    std::cout << "Enter the length of the room in inches: ";
     std::cin >> length;
 
-    std::cout << "Enter the width of the room in feet: ";
+
+    std::cout << "Enter the width of the room in inches: ";
     std::cin >> width;
 
     std::cout << std::endl;
+
+    double* result = new double[2];
+    result[0] = (length * inch_to_meter);
+    result[1] = (width* inch_to_meter);
+
+    return result;
 }
 
 // Function to get anchor locations within room from user input
@@ -72,15 +83,55 @@ std::vector<Eigen::Vector3d> getAnchors()
     return result;
 }
 
+// Function to read dimensions from a text file
+void readDimensions(std::string filename, double& length, double& width, std::vector<Eigen::Vector3d>& anchor_positions)
+{
+    std::ifstream inputFile;
+    inputFile.open(filename);
+
+    if (inputFile.is_open())
+    {
+        std::string line;
+        std::getline(inputFile, line);
+        sscanf_s(line.c_str(), "Room Dimensions: %lf, %lf", &length, &width);
+
+        for (int i = 0; i < 5; i++)
+        {
+            Eigen::Vector3d point;
+            std::getline(inputFile, line);
+            sscanf_s(line.c_str(), "Anchor %*d: %lf, %lf, %lf", &point[0], &point[1], &point[2]);
+            anchor_positions.push_back(point);
+        }
+
+        std::cout << "Dimensions read from " << filename << std::endl;
+    }
+    else
+    {
+        std::cout << "Error: Unable to open file" << std::endl;
+    }
+
+    inputFile.close();
+}
+
+
 // Function to save dimensions to a text file
-void saveDimensions(double length, double width, std::string filename)
+void saveDimensions(double length, double width, std::vector<Eigen::Vector3d> anchor_positions, std::string filename)
 {
     std::ofstream outputFile;
     outputFile.open(filename);
 
+    Eigen::Vector3d point_1 = anchor_positions[0];
+    Eigen::Vector3d point_2 = anchor_positions[1];
+    Eigen::Vector3d point_3 = anchor_positions[2];
+    Eigen::Vector3d point_4 = anchor_positions[3];
+
     if (outputFile.is_open())
     {
-        outputFile << length << " " << width << std::endl;
+        outputFile << "Room Dimensions: " << length << ", " << width << std::endl;
+        outputFile << "Anchor 1: " << point_1[0] << ", " << point_1[1] << ", " << point_1[2] << std::endl;
+        outputFile << "Anchor 2: " << point_2[0] << ", " << point_2[1] << ", " << point_2[2] << std::endl;
+        outputFile << "Anchor 3: " << point_3[0] << ", " << point_3[1] << ", " << point_3[2] << std::endl;
+        outputFile << "Anchor 4: " << point_4[0] << ", " << point_4[1] << ", " << point_4[2] << std::endl;
         std::cout << "Dimensions saved to " << filename << std::endl;
     }
     else
@@ -101,7 +152,7 @@ void drawTag(SDL_Renderer* renderer, float x, float y)
 }
 
 // calculates the primes points of intersection
-Eigen::Vector3d trilateration(float distance_1, float distance_2, float distance_3, Eigen::Vector3d point_1, Eigen::Vector3d point_2, Eigen::Vector3d point_3)
+Eigen::Vector3d trilateration(double distance_1, double distance_2, double distance_3, Eigen::Vector3d point_1, Eigen::Vector3d point_2, Eigen::Vector3d point_3)
 {
 
     // renames the anchor locations
@@ -135,42 +186,90 @@ Eigen::Vector3d trilateration(float distance_1, float distance_2, float distance
 
 // main code
 int main()
-{
-    // Ask for dimensions of the room in inches
-    std::cout << "Enter the dimensions of the room in inches (X Y): ";
-    double room_width_inches, room_height_inches;
-    std::cin >> room_width_inches >> room_height_inches;
+{   
+    // Declarations
+    Eigen::Vector3d point_1;
+    Eigen::Vector3d point_2;
+    Eigen::Vector3d point_3;
+    Eigen::Vector3d point_4;
+
+    double length;
+    double width;
+
+    std::cout << "Do you want to set up a new room? (Y/N)? ";
+    char choice_new_room;
+    std::cin >> choice_new_room;
+
+    if (choice_new_room == 'Y' || choice_new_room == 'y') {
+
+        // Ask for room dimensions from user
+        std::vector<double, double> room_dimensions = getDimensions();
+        length = room_dimensions[0];
+        width = room_dimensions[1];
+
+        // Ask for the anchor location from user
+        std::vector<Eigen::Vector3d> anchor_positions = getAnchors();
+        point_1 = anchor_positions[0];
+        point_2 = anchor_positions[1];
+        point_3 = anchor_positions[2];
+        point_4 = anchor_positions[3];
+
+        std::cout << "Do you want to save the room configuration (Y/N)?";
+        char choice_save;
+        std::cin >> choice_save;
+
+        if (choice_save == 'Y' || choice_save == 'y')
+        {
+            std::cout << "Enter the name of the room: ";
+            std::string save_filename;
+            std::cin >> save_filename;
+
+            // Check for existing file
+            struct stat buffer;
+            if (stat(save_filename.c_str(), &buffer) == 0) {
+
+                std::cout << "File already exists, try again";
+
+                return 0;
+            }
+
+            saveDimensions(length, width, anchor_positions, save_filename);
+
+        }
+    }
+    else if (choice_new_room == 'N' || choice_new_room == 'n') {
+
+        std::cout << "Enter the name of an existing room: ";
+        std::string read_filename;
+        std::cin >> read_filename;
+
+        // Checks for existing file
+        struct stat buffer;
+        if (stat(read_filename.c_str(), &buffer) != 0) {
+
+            std::cout << "File does not exist, try again";
+
+            return 0;
+        }
+
+        std::vector<Eigen::Vector3d> all_dimensions = readDimensions(read_filename);
+
+
+    }
+    else {
+
+        std::cout << "Invalid Input";
+
+        return 0;
+    }
 
     // Convert dimensions from inches to meters and set screen size
-    double room_width_meters = room_width_inches * inch_to_meter;
-    double room_height_meters = room_height_inches * inch_to_meter;
     const double screen_scale = 150.0;
-    double screen_width = room_width_meters * screen_scale;
-    double screen_height = room_height_meters * screen_scale;
-
-    // Ask for dimensions of the anchor points in inches
-    std::cout << "Enter the x, y, and z position of four anchor points in inches: " << std::endl;
-    double x, y, z;
-
-    // Convert each anchor point from inches to meters and replace variables
-    std::cout << "Anchor Point 1: ";
-    std::cin >> x >> y >> z;
-    Eigen::Vector3d point_1(x * inch_to_meter, y * inch_to_meter, z * inch_to_meter);
-
-    std::cout << "Anchor Point 2: ";
-    std::cin >> x >> y >> z;
-    Eigen::Vector3d point_2(x * inch_to_meter, y * inch_to_meter, z * inch_to_meter);
-
-    std::cout << "Anchor Point 3: ";
-    std::cin >> x >> y >> z;
-    Eigen::Vector3d point_3(x * inch_to_meter, y * inch_to_meter, z * inch_to_meter);
-
-    std::cout << "Anchor Point 4: ";
-    std::cin >> x >> y >> z;
-    Eigen::Vector3d point_4(x * inch_to_meter, y * inch_to_meter, z * inch_to_meter);
+    double screen_width = width * screen_scale;
+    double screen_height = length * screen_scale;
 
     // Print out results
-    std::cout << "Room dimensions (m): " << room_width_meters << " x " << room_height_meters << std::endl;
+    std::cout << "Room dimensions (m): " << width << " x " << length << std::endl;
     std::cout << "Screen size (pixels): " << screen_width << " x " << screen_height << std::endl;
     std::cout << "Anchor points (m): " << std::endl;
     std::cout << "  Point 1: " << point_1.transpose() << std::endl;
@@ -206,7 +305,6 @@ int main()
     sockaddr_in serverAddr;
     serverAddr.sin_family = AF_INET;
     serverAddr.sin_port = htons(1234);
-    serverAddr.sin_port = htons(1233);
     serverAddr.sin_addr.s_addr = INADDR_ANY;
 
     // checks if port binded properly
@@ -230,28 +328,15 @@ int main()
     }
 
     // Create a window and renderer
-    SDL_Window* window = SDL_CreateWindow("Object Movement", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, screen_width, screen_height, SDL_WINDOW_SHOWN);
+    SDL_Window* window = SDL_CreateWindow("Object Movement", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, screen_height, screen_width, SDL_WINDOW_SHOWN);
 
     // Create a renderer
     SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
 
     // Set the size and color of the object
-    int object_width = 7;
-    int object_height = 7;
-    SDL_Color object_color = { 210, 0 , 0, 255 };
-
-    // Defines variables for location coordinates
-    double x_location = 0;
-    double y_location = 0;
-    double z_location = 0;
-
-    // Defines position and velocity variables
-    float imuX = 0;
-    float imuY = 0;
-    float imuZ = 0;
-    float vx = 0;
-    float vy = 0;
-    float vz = 0;
+    int object_width = 5;
+    int object_height = 5;
+    SDL_Color object_color = { 255, 0 , 0, 255 };
 
     // data collection and display loop
     bool quit = false;
@@ -260,56 +345,22 @@ int main()
         if (recvLen > 0) {
             std::string str(buffer, recvLen);
 
-            // reads incoming string into data
-            std::vector<double> data;
-            float distance_1 = 0;
-            float distance_2 = 0;
-            float distance_3 = 0;
-            float distance_4 = 0;
-            float roll = 0;
-            float pitch = 0;
-            float yaw = 0;
-            float rawAx = 0;
-            float rawAy = 0;
-            float rawAz = 0;
-            float dt = 0;
-
+            // converts incoming string into 4 distance doubles
             size_t start = str.find("[") + 1;
             size_t end = str.find(",");
+            double distance_1 = std::stod(str.substr(start, end - start));
 
-            while (str.find("]", start) != -1) {
-                data.push_back(std::stof(str.substr(start, end - start)));
-                start = end + 1;
-                end = str.find(",", start);
-                if (end == -1) {
-                    end = str.find("]", start);
-                }
-            }
-            for (int i = 0; i < data.size(); i++) {
-                std::cout << data[i] << ", ";
-            }
-            std::cout << std::endl;
+            start = end + 1;
+            end = str.find(",", start);
+            double distance_2 = std::stod(str.substr(start, end - start));
 
+            start = end + 1;
+            end = str.find(",", start);
+            double distance_3 = std::stod(str.substr(start, end - start));
 
-            if (data.size() == 4) {
-                distance_1 = data[0];
-                distance_2 = data[1];
-                distance_3 = data[2];
-                distance_4 = data[3];
-            }
-            else if (data.size() == 7) {
-                roll = data[0]; // degrees
-                pitch = data[1]; // degrees
-                yaw = data[2]; // degrees
-                rawAx = data[3]; // mg
-                rawAy = data[4]; // mg
-                rawAz = data[5]; // mg
-                dt = data[6]/1000000; // s
-            }
-
-            /***************/
-            /***** UWB *****/
-            /***************/
+            start = end + 1;
+            end = str.find("]", start);
+            double distance_4 = std::stod(str.substr(start, end - start));
 
             // calculates the prime points to determine the location given overestimates
             Eigen::Vector3d point_1_prime = trilateration(distance_1, distance_2, distance_3, point_1, point_2, point_3);
@@ -318,12 +369,12 @@ int main()
             Eigen::Vector3d point_4_prime = trilateration(distance_1, distance_4, distance_3, point_1, point_4, point_3);
 
             // finds location of tag in (x,y,z)
-            x_location = (point_1_prime(0) + point_2_prime(0) + point_3_prime(0) + point_4_prime(0)) / 4;
-            y_location = (point_1_prime(1) + point_2_prime(1) + point_3_prime(1) + point_4_prime(1)) / 4;
-            z_location = (point_1_prime(2) + point_2_prime(2) + point_3_prime(2) + point_4_prime(2)) / 4;
+            double x_location = (point_1_prime(0) + point_2_prime(0) + point_3_prime(0) + point_4_prime(0)) / 4;
+            double y_location = (point_1_prime(1) + point_2_prime(1) + point_3_prime(1) + point_4_prime(1)) / 4;
+            double z_location = (point_1_prime(2) + point_2_prime(2) + point_3_prime(2) + point_4_prime(2)) / 4;
 
             // writes the location data to the console
-            //std::cout << "x: " << x_location << ", y: " << y_location << ", z: " << z_location << std::endl;
+            std::cout << "x: " << x_location << ", y: " << y_location << ", z: " << z_location << std::endl;
 
             // Handle events (such as window close)
             SDL_Event event;
@@ -338,76 +389,18 @@ int main()
             // Write location data to file
             outFile << "X Position: " << x_location << ", Y Position: " << y_location << std::endl;
 
-            /***************/
-            /***** IMU *****/
-            /***************/
-
-            // calculating orientation
-            float zerodir = 180.0; // compass direction (degrees from North) where yaw is 0
-            float compass = zerodir - yaw;
-            if (compass < 0) {
-                compass += 360;
-            }
-            float angle1 = 90 - roll; // tilt up = positive, tilt down = negative
-            float angle2 = -pitch; // right = positive, left = negative
-
-            //std::cout << "  Compass: " << compass << std::endl;
-            //std::cout << "  Angle 1: " << angle1 << std::endl;
-            //std::cout << "  Angle 2: " << angle2 << std::endl;
-
-            double PI = M_PI;
-            // calculating gravity components (mg)
-            float gx = -1000 * sin(pitch * PI / 180);
-            float gy = 1000 * cos(pitch * PI / 180) * sin(roll * PI / 180);
-            float gz = 1000 * cos(pitch * PI / 180) * cos(roll * PI / 180);
-
-            // calculating acceleration without gravity, converting from mg to m/s^2, & switching coordinate system
-            float ax = (rawAx - gx) / 9810;
-            float ay = (rawAz - gz) / 9810;
-            float az = (rawAy - gy) / 9810;
-
-            // update velocity (m/s)
-            vx += ax * dt;
-            vy += ay * dt;
-            vz += az * dt;
-
-            // update position (m)
-            imuX += vx * dt;
-            imuY += vy * dt;
-            imuZ += vz * dt;
-
-            std::cout << "  Ax: " << ax << "  Ay: " << ay << "  Az: " << az << std::endl;
-            std::cout << "  Vx: " << vx << "  Vy: " << vy << "  Vz: " << vz << std::endl;
-            std::cout << "  X: " << imuX << "  Y: " << imuY << "  Z: " << imuZ << std::endl;
-
-            /***************/
-            /***** Vis *****/
-            /***************/
-
             // Clear the screen
             SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
             SDL_RenderClear(renderer);
 
             // Convert the object position from meters to pixels
-            x_location = imuX;
-            y_location = imuY;
-            int x_location_pixel = static_cast<int>(x_location * screen_scale);
-            int y_location_pixel = static_cast<int>(y_location * screen_scale);
-            //x_location_pixel = 40;
-            //y_location_pixel = 40;
-
-            // Calculate line for orientation, currently assuming that North is in the positive x direction
-            int length = 20;
-            int x1 = x_location_pixel + length * cos(compass * PI / 180);
-            int y1 = y_location_pixel + length * sin(compass * PI / 180);
+            int x_location_pixel = static_cast<int>(x_location * 150);
+            int y_location_pixel = static_cast<int>(y_location * 150);
 
             // Draw the object
             SDL_Rect object_rect = { x_location_pixel, y_location_pixel, object_width, object_height };
             SDL_SetRenderDrawColor(renderer, object_color.r, object_color.g, object_color.b, object_color.a);
             SDL_RenderFillRect(renderer, &object_rect);
-
-            // Draw line for orientation
-            SDL_RenderDrawLine(renderer, x_location_pixel, y_location_pixel, x1, y1);
 
             // Presents the rendereer to the screen
             SDL_RenderPresent(renderer);
