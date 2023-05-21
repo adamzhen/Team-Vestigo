2d74e4af52028152ef5b81870d0ba2fcadfb565f
Hybrinter <102422753+Hybrinter@users.noreply.github.com>
2023-05-21 11:52:42 -0500

    Update UWB_Tag.ino

--- UWB-Code/UWB_Tag/UWB_Tag.ino
+++ UWB-Code/UWB_Tag/UWB_Tag.ino
@@ -278,78 +278,11 @@ void twr_receiver_mode(int key)
   }
 }
 
-void setup() 
-{
-  Serial.begin(115200);
+/******************************************
+************ ADVANCED RANGING ************
+******************************************/
 
-  // WiFi Connection
-  Serial.print("Connecting to ");
-  Serial.println(ssid);
-  WiFi.begin(ssid, password);
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(1000);
-    Serial.println("Connecting to Wifi... ");
-  }
-  Serial.println("Wifi connected");
-  delay(2000);
-
-  UART_init();
-  test_run_info((unsigned char *)APP_NAME);
-
-  /* Configure SPI rate, DW3000 supports up to 38 MHz */
-  /* Reset DW IC */
-  spiBegin(PIN_IRQ, PIN_RST);
-  spiSelect(PIN_SS);
-
-  delay(2); // Time needed for DW3000 to start up (transition from INIT_RC to IDLE_RC, or could wait for SPIRDY event)
-
-  while (!dwt_checkidlerc()) // Need to make sure DW IC is in IDLE_RC before proceeding 
-  {
-    UART_puts("IDLE FAILED\r\n");
-    while (1) ;
-  }
-
-  if (dwt_initialise(DWT_DW_INIT) == DWT_ERROR)
-  {
-    UART_puts("INIT FAILED\r\n");
-    while (1) ;
-  }
-
-  // Enabling LEDs here for debug so that for each TX the D1 LED will flash on DW3000 red eval-shield boards.
-  dwt_setleds(DWT_LEDS_ENABLE | DWT_LEDS_INIT_BLINK);
-
-  /* Configure DW IC. See NOTE 6 below. */
-  if(dwt_configure(&config)) // if the dwt_configure returns DWT_ERROR either the PLL or RX calibration has failed the host should reset the device
-  {
-    UART_puts("CONFIG FAILED\r\n");
-    while (1) ;
-  }
-
-  /* Configure the TX spectrum parameters (power, PG delay and PG count) */
-  dwt_configuretxrf(&txconfig_options);
-
-  /* Apply default antenna delay value. See NOTE 2 below. */
-  dwt_setrxantennadelay(RX_ANT_DLY);
-  dwt_settxantennadelay(TX_ANT_DLY);
-
-  /* Set expected response's delay and timeout. See NOTE 1 and 5 below.
-  * As this example only handles one incoming frame with always the same delay and timeout, those values can be set here once for all. */
-  dwt_setrxaftertxdelay(POLL_TX_TO_RESP_RX_DLY_UUS);
-  dwt_setrxtimeout(RESP_RX_TIMEOUT_UUS);
-
-  /* Next can enable TX/RX states output on GPIOs 5 and 6 to help debug, and also TX/RX LEDs
-  * Note, in real low power applications the LEDs should not be used. */
-  dwt_setlnapamode(DWT_LNA_ENABLE | DWT_PA_ENABLE);
-
-  Serial.println("Checks");
-
-  for (int i = 1; i <= 12; ++i) {
-    keys.push_back(std::make_pair(i, distance_data[i].size()));
-  }
-  
-}
-
-void loop() 
+void advancedRanging()
 {
   // Distance reset
   bool looping = true;
@@ -501,73 +434,86 @@ void loop()
   }
 }
 
+/**************************************
+************ PROGRAM SETUP ************
+**************************************/
 
-/*****************************************************************************************************************************************************
- * NOTES:
- *
- * 1. The single-sided two-way ranging scheme implemented here has to be considered carefully as the accuracy of the distance measured is highly
- *    sensitive to the clock offset error between the devices and the length of the response delay between frames. To achieve the best possible
- *    accuracy, this response delay must be kept as low as possible. In order to do so, 6.8 Mbps data rate is used in this example and the response
- *    delay between frames is defined as low as possible. The user is referred to User Manual for more details about the single-sided two-way ranging
- *    process.  NB:SEE ALSO NOTE 11.
- *
- *    Initiator: |Poll TX| ..... |Resp RX|
- *    Responder: |Poll RX| ..... |Resp TX|
- *                   ^|P RMARKER|                    - time of Poll TX/RX
- *                                   ^|R RMARKER|    - time of Resp TX/RX
- *
- *                       <--TDLY->                   - POLL_TX_TO_RESP_RX_DLY_UUS (RDLY-RLEN)
- *                               <-RLEN->            - RESP_RX_TIMEOUT_UUS   (length of response frame)
- *                    <----RDLY------>               - POLL_RX_TO_RESP_TX_DLY_UUS (depends on how quickly responder can turn around and reply)
- *
- *
- * 2. The sum of the values is the TX to RX antenna delay, this should be experimentally determined by a calibration process. Here we use a hard coded
- *    value (expected to be a little low so a positive error will be seen on the resultant distance estimate). For a real production application, each
- *    device should have its own antenna delay properly calibrated to get good precision when performing range measurements.
- * 3. The frames used here are Decawave specific ranging frames, complying with the IEEE 802.15.4 standard data frame encoding. The frames are the
- *    following:
- *     - a poll message sent by the initiator to trigger the ranging exchange.
- *     - a response message sent by the responder to complete the exchange and provide all information needed by the initiator to compute the
- *       time-of-flight (distance) estimate.
- *    The first 10 bytes of those frame are common and are composed of the following fields:
- *     - byte 0/1: frame control (0x8841 to indicate a data frame using 16-bit addressing).
- *     - byte 2: sequence number, incremented for each new frame.
- *     - byte 3/4: PAN ID (0xDECA).
- *     - byte 5/6: destination address, see NOTE 4 below.
- *     - byte 7/8: source address, see NOTE 4 below.
- *     - byte 9: function code (specific values to indicate which message it is in the ranging process).
- *    The remaining bytes are specific to each message as follows:
- *    Poll message:
- *     - no more data
- *    Response message:
- *     - byte 10 -> 13: poll message reception timestamp.
- *     - byte 14 -> 17: response message transmission timestamp.
- *    All messages end with a 2-byte checksum automatically set by DW IC.
- * 4. Source and destination addresses are hard coded constants in this example to keep it simple but for a real product every device should have a
- *    unique ID. Here, 16-bit addressing is used to keep the messages as short as possible but, in an actual application, this should be done only
- *    after an exchange of specific messages used to define those short addresses for each device participating to the ranging exchange.
- * 5. This timeout is for complete reception of a frame, i.e. timeout duration must take into account the length of the expected frame. Here the value
- *    is arbitrary but chosen large enough to make sure that there is enough time to receive the complete response frame sent by the responder at the
- *    6.8M data rate used (around 200 µs).
- * 6. In a real application, for optimum performance within regulatory limits, it may be necessary to set TX pulse bandwidth and TX power, (using
- *    the dwt_configuretxrf API call) to per device calibrated values saved in the target system or the DW IC OTP memory.
- * 7. dwt_writetxdata() takes the full size of the message as a parameter but only copies (size - 2) bytes as the check-sum at the end of the frame is
- *    automatically appended by the DW IC. This means that our variable could be two bytes shorter without losing any data (but the sizeof would not
- *    work anymore then as we would still have to indicate the full length of the frame to dwt_writetxdata()).
- * 8. We use polled mode of operation here to keep the example as simple as possible but all status events can be used to generate interrupts. Please
- *    refer to DW IC User Manual for more details on "interrupts". It is also to be noted that STATUS register is 5 bytes long but, as the event we
- *    use are all in the first bytes of the register, we can use the simple dwt_read32bitreg() API call to access it instead of reading the whole 5
- *    bytes.
- * 9. The high order byte of each 40-bit time-stamps is discarded here. This is acceptable as, on each device, those time-stamps are not separated by
- *    more than 2**32 device time units (which is around 67 ms) which means that the calculation of the round-trip delays can be handled by a 32-bit
- *    subtraction.
- * 10. The user is referred to DecaRanging ARM application (distributed with EVK1000 product) for additional practical example of usage, and to the
- *     DW IC API Guide for more details on the DW IC driver functions.
- * 11. The use of the clock offset value to correct the TOF calculation, significantly improves the result of the SS-TWR where the remote
- *     responder unit's clock is a number of PPM offset from the local initiator unit's clock.
- *     As stated in NOTE 2 a fixed offset in range will be seen unless the antenna delay is calibrated and set correctly.
- * 12. In this example, the DW IC is put into IDLE state after calling dwt_initialise(). This means that a fast SPI rate of up to 20 MHz can be used
- *     thereafter.
- * 13. Desired configuration by user may be different to the current programmed configuration. dwt_configure is called to set desired
- *     configuration.
- ****************************************************************************************************************************************************/
+void setup() 
+{
+  Serial.begin(115200);
+
+  // WiFi Connection
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(1000);
+    Serial.println("Connecting to Wifi... ");
+  }
+  Serial.println("Wifi connected");
+  delay(2000);
+
+  UART_init();
+  test_run_info((unsigned char *)APP_NAME);
+
+  /* Configure SPI rate, DW3000 supports up to 38 MHz */
+  /* Reset DW IC */
+  spiBegin(PIN_IRQ, PIN_RST);
+  spiSelect(PIN_SS);
+
+  delay(2); // Time needed for DW3000 to start up (transition from INIT_RC to IDLE_RC, or could wait for SPIRDY event)
+
+  while (!dwt_checkidlerc()) // Need to make sure DW IC is in IDLE_RC before proceeding 
+  {
+    UART_puts("IDLE FAILED\r\n");
+    while (1) ;
+  }
+
+  if (dwt_initialise(DWT_DW_INIT) == DWT_ERROR)
+  {
+    UART_puts("INIT FAILED\r\n");
+    while (1) ;
+  }
+
+  // Enabling LEDs here for debug so that for each TX the D1 LED will flash on DW3000 red eval-shield boards.
+  dwt_setleds(DWT_LEDS_ENABLE | DWT_LEDS_INIT_BLINK);
+
+  /* Configure DW IC. See NOTE 6 below. */
+  if(dwt_configure(&config)) // if the dwt_configure returns DWT_ERROR either the PLL or RX calibration has failed the host should reset the device
+  {
+    UART_puts("CONFIG FAILED\r\n");
+    while (1) ;
+  }
+
+  /* Configure the TX spectrum parameters (power, PG delay and PG count) */
+  dwt_configuretxrf(&txconfig_options);
+
+  /* Apply default antenna delay value. See NOTE 2 below. */
+  dwt_setrxantennadelay(RX_ANT_DLY);
+  dwt_settxantennadelay(TX_ANT_DLY);
+
+  /* Set expected response's delay and timeout. See NOTE 1 and 5 below.
+  * As this example only handles one incoming frame with always the same delay and timeout, those values can be set here once for all. */
+  dwt_setrxaftertxdelay(POLL_TX_TO_RESP_RX_DLY_UUS);
+  dwt_setrxtimeout(RESP_RX_TIMEOUT_UUS);
+
+  /* Next can enable TX/RX states output on GPIOs 5 and 6 to help debug, and also TX/RX LEDs
+  * Note, in real low power applications the LEDs should not be used. */
+  dwt_setlnapamode(DWT_LNA_ENABLE | DWT_PA_ENABLE);
+
+  for (int i = 1; i <= 12; ++i) {
+    keys.push_back(std::make_pair(i, distance_data[i].size()));
+  }
+  
+}
+
+/*************************************
+************ PROGRAM LOOP ************
+*************************************/
+
+void loop() 
+{
+  advancedRanging();
+}
+
+
