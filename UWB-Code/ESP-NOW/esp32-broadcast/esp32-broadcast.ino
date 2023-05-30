29497250e8f716cdc2f37a5694fff4df46dab8e4
Hybrinter <kampwerthaiden@duck.com>
2023-05-30 15:03:46 -0500

    Create esp32-broadcast.ino

---
+++ UWB-Code/ESP-NOW/esp32-broadcast/esp32-broadcast.ino
@@ -0,0 +1,112 @@
+#include <WiFi.h>
+#include <essp_now.h>
+
+bool buttonDown = false;
+bool ledOn = false;
+
+void formatMacAddress(const uint8_t *macAddr, char *buffer, int maxLength) {
+  snprintf(buffer, maxLength, "%02x:%02x:%02x:%02x:%02x:%02x", macAddr[0], macAddr[1], macAddr[2], macAddr[3], macAddr[4], macAddr[5])
+}
+
+void receiveCallback(const uint8_t *macAddr, const uint8_t *data, int dataLen) {
+  char buffer[ESP_NOW_MAX_DATA_LEN + 1];
+  int msgLen = min(ESP_NOW_MAX_DATA_LEN, dataLen);
+  strncpy(buffer, (const char *) data, msgLen);
+
+  buffer[msgLen] = 0;
+
+  char macStr[18];
+  formatMacAddress(macAddr, macStr, 18);
+
+  Serial.printf("Received message from %s - %s\n", macStr, buffer);
+
+  if (strcmp("on", buffer) == 0) {
+    ledOn = true;
+  }
+  else {
+    ledOn = false;
+  }
+
+  Serial.print("Led ON: ");
+  Serial.println(ledOn);
+}
+
+void sentCallback(const uint8_t *macAddr, esp_now_send_status_t status) {
+  char macStr[18];
+  formatMacAddress(macAddr, macStr, 18);
+  Serial.print("Last Packet Sent to: ");
+  Serial.println(macStr);
+  Serial.print("Last Packet Send Status: ");
+  Serial.println(status == ESP_NOW_SEND_SUCCESS ? "Delivery Success" : "Delivery Fail");
+}
+
+void broadcast(const String &message) {
+  uint8_t broadcastAddress[] = {0xFF, 0xFF, 0xFF, 0xFF, 0xFF,0xFF};
+  esp_now_peer_info_t peerInfo = {};
+  memcpy(&peerInfo.peer_addr, broadcastAddress, 6);
+  if (!esp_now_is_peer_exist(broadcastAddress)) {
+    esp_now_add_peer(&peerInfo);
+  }
+
+  esp_err_t result = esp_now_send(boradcastAddress, (const uint8_t *) message.c_str(), message.length());
+
+  if (result == ESP_OK) {
+    Serial.println("Broadcast message success");
+  }
+  else if (result == ESP_ERR_ESPNOW_NOT_INIT) {
+    Serial.println("ESP-NOW not Init");
+  }
+  else if (result == ESP_ERR_ESPNOW_ARG) {
+    Serial.println("Invalid Argument");
+  }
+  else if (result == ESP_ERR_ESPNOW_INTERNAL) {
+    Serial.println("Internal Error");
+  }
+  else if (result == ESP_ERR_ESPNOW_NO_MEM) {
+    Serial.println("No Mem")
+  }
+  else if (result == ESP_ERR_ESPNOW_NOT_FOUND) {
+    Serial.println("Peer Not Found");
+  }
+  else {
+    Serial.println("Unknown Error")
+  }
+}
+
+void setup() {
+  Serial.begin(115200);
+  delay(1000);
+
+  WiFi.mode(WIFI_STA);
+  Serial.println("ESP-NOW Broadcast Demo");
+
+  Serial.print("MAC Address: ");
+  Serial.println(WiFi.macAddress());
+
+  WiFi.disconnect();
+
+  if (esp_now_init() == ESP_OK) {
+    Serial.println("ESP-NOW Init Success");
+    esp_now_register_recv_cb(receiveCallback);
+    eso_now_register_send_cb(sentCallback);
+  }
+  else {
+    Serial.println("ESP-NOW Init Failed");
+    delay(3000);
+    ESP.restart();
+  }
+}
+
+void loop() {
+
+  ledOn = !ledOn;
+
+  if (ledOn) {
+    broadcast("on");
+  } 
+  else {
+    broadcast("off");
+  }
+
+  delay(1000);
+}
