78730260498c16030dcc3b353bf42417fbee97fd
Hybrinter <kampwerthaiden@duck.com>
2023-05-30 14:19:42 -0500

    Create esp32_responder.ino

---
+++ UWB-Code/ESP-NOW/esp32_responder/esp32_responder.ino
@@ -0,0 +1,43 @@
+#include <esp_now.h>
+#include <WiFi.h>
+
+typedef struct struct_message {
+  char a[32];
+  int b;
+  float c;
+  bool d;
+} struct_message;
+
+struct_message myData;
+
+void OnDataRecv(const uint8_t * mac, const uint8_t *incomingData, int len) {
+  memcpy(&myData, incomingData, sizeof(myData));
+  Serial.print("Data Received: ");
+  Serial.println(len);
+  Serial.print("Character Value: ");
+  Serial.println(myData.a);
+  Serial.print("Integer Value: ");
+  Serial.println(myData.b);
+  Serial.print("Float Value: ");
+  Serial.println(myData.c);
+  Serial.print("Boolean Value: ");
+  Serial.println(myData.d);
+  Serial.println();
+}
+
+void setup() {
+  Serial.beign(115200);
+
+  WiFi.mode(WIFI_STA);
+
+  if (esp_now_init() != ESP_OK) {
+    Serial.println("Error initializing ESP-NOW");
+    return;
+  }
+
+  esp_now_register_recv_cb(OnDataRecv);
+}
+
+void loop() {
+
+}
