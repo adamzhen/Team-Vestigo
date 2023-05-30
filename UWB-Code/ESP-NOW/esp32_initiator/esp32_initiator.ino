4832d6f2a122d6f7436a0893f162a16337bd5bce
Hybrinter <kampwerthaiden@duck.com>
2023-05-30 14:30:17 -0500

    Update esp32_initiator.ino

--- UWB-Code/ESP-NOW/esp32_initiator/esp32_initiator.ino
+++ UWB-Code/ESP-NOW/esp32_initiator/esp32_initiator.ino
@@ -21,7 +21,7 @@ esp_now_peer_info_t peerInfo;
 
 void OnDataSent(const uint8_t *mac_addr, esp_now_send_status_t status) {
   Serial.print("\r\nLast Packet Send Status:\t");
-  Serial.print(status == ESP_NOW_SEND_SUCCESS ? "Delivery Success" : "Delivery Fail");
+  Serial.println(status == ESP_NOW_SEND_SUCCESS ? "Delivery Success" : "Delivery Fail");
 }
 
 void setup() {
