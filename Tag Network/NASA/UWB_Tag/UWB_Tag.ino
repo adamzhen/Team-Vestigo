45a01e7ea0de2d47689aff0a99c90e0e721f3839
Hybrinter <kampwerthaiden@duck.com>
2023-06-24 13:34:54 -0500

    Syntax Changes

--- Tag Network/NASA/UWB_Tag/UWB_Tag.ino
+++ Tag Network/NASA/UWB_Tag/UWB_Tag.ino
@@ -8,24 +8,6 @@
 #include <SPI.h>
 #include <esp_now.h>
 
-void printKeysAndDistances() {
-  for (const auto& key : keys) {
-    Serial.print("Key: ");
-    Serial.print(key.first);
-    Serial.print(", Distances: ");
-
-    if (key.second.empty()) {
-      Serial.println("No distances recorded");
-    } else {
-      for (const auto& distance : key.second) {
-        Serial.print(distance);
-        Serial.print(" ");
-      }
-      Serial.println();
-    }
-  }
-}
-
 /******************************************
 ******** GEN VARIABLES AND STRUCTS ********
 ******************************************/
@@ -421,7 +403,21 @@ void advancedRanging() {
           it->second.push_back(distance);
       }
 
-      printKeysAndDistances();
+      for (const auto& key : keys) {
+        Serial.print("Key: ");
+        Serial.print(key.first);
+        Serial.print(", Distances: ");
+
+        if (key.second.empty()) {
+          Serial.println("No distances recorded");
+        } else {
+          for (const auto& distance : key.second) {
+            Serial.print(distance);
+            Serial.print(" ");
+          }
+          Serial.println();
+        }
+      }
 
       // counter
       int unique_distance_counter = 0;
