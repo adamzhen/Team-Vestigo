9e4ba45fb12cb4183da0761c836dd711de1b52c4
adamzhen <adzheng@tamu.edu>
2023-09-18 14:22:46 -0500

    VS files

--- Visualization Tool/VisualizationToolDev/main.cpp
+++ Visualization Tool/VisualizationToolDev/main.cpp
@@ -1,11 +1,13 @@
 #include <iostream>
 #include <string>
 #include <cmath>
-#include <SDL.h>
 
 int main(int argc, char* argv[])
 {
 	std::cout << "good morning" << std::endl;
+	std::string name;
+	std::cin >> name;
+	std::cout << "hello " << name << std::endl;
 
 	return 0;
 }
